ac5c74d5d0b444ae1d8dd4ce9edf4574e3e5ffa1
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2022-03-23 17:29:35 +0100

    Tip EmptyListComponent

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+/*Als lijst leeg is gebruik de ListEmptyComponent die een message weergeeft zoals "Geen todo's" => ListEmptyComponent ={() => <Text>Geen todo's gevonden</Text>} */
+
 const TodoScreen = () => {
   return (
     <View>
